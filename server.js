2d09350d89240c56949569119f03872e003f168e
AkshayAyyanchira <aayyanch@uncc.edu>
2017-11-14 00:06:13 -0500

    one import and 3 lines to create a server without any external dependencies yet installed

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ b = 10;
 c = a*b;
 
 console.log('Addition of '+a+' and '+b+' is '+c);
+
+// Now to create a server you need http package. So create a variable which is http handle
+
+var http = require('http');
+
+http.createServer(function(req,res){
+    console.log('You just created a server which listens to port 3000');
+}).listen(3000)
